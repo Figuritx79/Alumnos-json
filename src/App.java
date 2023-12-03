f8d64869443090d78d2bb02fcf2583ba1b488b3e
Enrique Gonzalez <gonherenrique@gmail.com>
2023-12-03 09:33:29 -0600

    Ya solicita las edades, tambien el archivo que se crea(json) formateado correctamente

--- src/App.java
+++ src/App.java
@@ -3,9 +3,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import org.json.JSONArray;
-// import org.json.JSONArray;1
 import org.json.JSONObject;
 
 public class App {
@@ -14,6 +12,7 @@ public class App {
 
     private static String nombresAlumnos;
     private static int edades;
+    private static String[][] materiasCalificaciones;
 
     public static void main(String[] args) throws Exception {
         // Creamos el buffer para escribir lo que le vayamos a pasar
@@ -30,29 +29,37 @@ public class App {
             // Creamos el fileWriter para escribir en un archivo. En este caso es un .json
             FileWriter fw = new FileWriter(file);
 
+            // Le decimos que el buffer va a escribir en fw
             bw = new BufferedWriter(fw);
 
             System.out.println("Hola, Que quieres realizar hoy?");
 
             boolean repetir = true;
+            int identFactor = 1;
+
+            JSONObject alumnos;
+            JSONObject datosAlumnos = new JSONObject();
+            JSONArray listaAlumnos = new JSONArray();
 
             do {
-                JSONObject jsonObject = new JSONObject();
-
-                JSONArray jsonArray = new JSONArray();
-
+                alumnos = new JSONObject();
                 System.out.println("1.Agregar alumno, 2.Buscar alumno, 3.Mostrar alumnos");
                 String opcionRealizar = scanner.next();
 
                 opcionARealizar(opcionRealizar);
-                jsonObject.put("Nombre", nombresAlumnos);
 
-                jsonArray.put(jsonObject);
+                alumnos.put("Nombres", nombresAlumnos);
+                alumnos.put("Edad", edades);
 
-                bw.write(jsonArray.toString(6));
+                datosAlumnos.put("Alumno" + identFactor, alumnos);
 
+                identFactor++;
                 repetir = repetirAccion();
             } while (repetir);
+
+            listaAlumnos.put(datosAlumnos);
+            // Escribimos en el buffer
+            bw.write(listaAlumnos.toString());
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
@@ -75,6 +82,7 @@ public class App {
             case "1":
                 System.out.println("Opcion 1");
                 nombreAlumno();
+                edadAlumno();
                 break;
             case "2":
                 System.out.println("Opcion 2");
@@ -89,7 +97,7 @@ public class App {
         }
     }
 
-    // Preguntar si quiere preguntar
+    // Preguntar si quiere hacer otra cosa
     private static boolean repetirAccion() {
         System.out.println("-------------------------------------");
         System.out.println("Quieres volver a escoger una opcion? [Y/N]");
@@ -103,10 +111,15 @@ public class App {
 
     // Pregutar su nombre
     private static void nombreAlumno() {
-        System.out.println("Ingresa tu nombre completo :)");
+        System.out.println("Ingresa el nombre completo del alumno :)");
         nombresAlumnos = scanner.next();
     }
+
     // Pedir su edad
+    private static void edadAlumno() {
+        System.out.println("Ingresa la edad del alumno :)");
+        edades = scanner.nextInt();
+    }
 
     // Usar un arreglo bidimensional para almacenar materias y calificaciones
 }
